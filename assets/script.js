07451352cafe51106a6d0e1d580a7d96ce17979e
elizabeth padron <elizabethpadron02@gmail.com>
2023-10-31 00:42:38 -0500

    added search engine

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,9 @@
-const searchInput = document.getElementById('search-input');
-document.getElementById('search-form').addEventListener('submit', function (event) {
-    event.preventDefault();
-    const city = searchInput.value.trim();
 
-    fetchWeatherData(city);
+const searchInput = document.getElementById('search-input');
+document.getElementById('search-button').addEventListener('click', function (event) {
+  event.preventDefault();
+  const city = "Atlanta";
+  fetchWeatherData(city);
 });
 
-
-const apiKey ="accef3746f34716977dc641e803d736d"
+const apiKey = "accef3746f34716977dc641e803d736d";
