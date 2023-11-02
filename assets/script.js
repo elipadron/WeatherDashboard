a8b66d3a14004b5ca1c027d33f61f600162cdbce
elizabeth padron <elizabethpadron02@gmail.com>
2023-11-02 17:31:14 -0500

    added styling to the header

--- assets/script.js
+++ assets/script.js
@@ -7,3 +7,4 @@ document.getElementById('search-button').addEventListener('click', function (eve
 });
 
 const apiKey = "accef3746f34716977dc641e803d736d";
+api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key} 
